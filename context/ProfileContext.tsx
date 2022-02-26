e4e915fb6b8d700e4cc42ad9ed80832f5a110c22
German Cocca <coccagerman@gmail.com>
2022-02-26 11:10:44 -0300

    .

--- context/ProfileContext.tsx
+++ context/ProfileContext.tsx
@@ -5,18 +5,18 @@ interface ProfileContext {
     userData: UserData | null,
     favoriteCocktails: Array<Cocktail>,
     publishedRecipes: Array<Cocktail>,
-    fetchUserData: (user: any) => void,
-    fetchFavoriteCocktails: (userData: UserData | null) => void,
-    fetchPublishedRecipes: (user: any) => void
+    fetchUserData: (user: any) => Promise<void>,
+    fetchFavoriteCocktails: (userData: UserData | null) => Promise<void>,
+    fetchPublishedRecipes: (user: any) => Promise<void>,
 }
 
 const defaultState = {
     userData: null,
     favoriteCocktails: [],
     publishedRecipes: [],
-    fetchUserData: (user: any): void => {throw new Error('fetchUserData function must be overridden')},
-    fetchFavoriteCocktails: (userData: UserData | null): void => {throw new Error('fetchFavoriteCocktails function must be overridden')},
-    fetchPublishedRecipes: (user: any): void => {throw new Error('fetchPublishedRecipes function must be overridden')}
+    fetchUserData: (user: any) => Promise,
+    fetchFavoriteCocktails: (userData: UserData | null) => Promise,
+    fetchPublishedRecipes: (user: any) => Promise
 }
 
 const ProfileContext = createContext<ProfileContext>(defaultState)
